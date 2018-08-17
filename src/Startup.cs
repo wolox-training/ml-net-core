ddaca130b0331dc56f4d3f6100b92a65b78b7d29
mikolaslorant <lmikolas@itba.edu.ar>
2018-08-17 17:13:14 -0300

    fixed namespaces

--- src/Startup.cs
+++ src/Startup.cs
@@ -10,7 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace src
+namespace MlNetCore
 {
     public class Startup
     {
--- test/UnitTest1.cs
+++ test/UnitTest1.cs
@@ -1,7 +1,7 @@
 using System;
 using Xunit;
 
-namespace test
+namespace MlNetCore.Test
 {
     public class UnitTest1
     {
